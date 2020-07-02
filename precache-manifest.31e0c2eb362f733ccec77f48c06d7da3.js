2c7e400b30bf37f5fd0a3539153dba36d7794630
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-07-02 14:46:30 +0100

    Updates

---
+++ precache-manifest.31e0c2eb362f733ccec77f48c06d7da3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4aa542f08747dd9933eeff95061e210",
+    "url": "/react-milestone-3/index.html"
+  },
+  {
+    "revision": "a1bd1ea105e3c24f25b2",
+    "url": "/react-milestone-3/static/css/main.a7f2561a.chunk.css"
+  },
+  {
+    "revision": "e612352099c9bec53438",
+    "url": "/react-milestone-3/static/js/2.fe0b1432.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milestone-3/static/js/2.fe0b1432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bd1ea105e3c24f25b2",
+    "url": "/react-milestone-3/static/js/main.c31b2729.chunk.js"
+  },
+  {
+    "revision": "ccc6cc56101f4ea14900",
+    "url": "/react-milestone-3/static/js/runtime-main.e2b5cdd6.js"
+  },
+  {
+    "revision": "f4f1b5af829bf387f4b40877b336455a",
+    "url": "/react-milestone-3/static/media/cow.f4f1b5af.png"
+  },
+  {
+    "revision": "95f7ec7fc393d67d22704d42745f34d5",
+    "url": "/react-milestone-3/static/media/dog.95f7ec7f.png"
+  },
+  {
+    "revision": "55815840b5b381b2d11d7c7e1282a96d",
+    "url": "/react-milestone-3/static/media/fox.55815840.png"
+  },
+  {
+    "revision": "d364485f4910bebdebfe86f462437f5a",
+    "url": "/react-milestone-3/static/media/monkey.d364485f.png"
+  },
+  {
+    "revision": "b0cad456f6dd20f05eb60bbbdd17fd85",
+    "url": "/react-milestone-3/static/media/mouse.b0cad456.png"
+  },
+  {
+    "revision": "0ed03c8cec1e3d9dafe20653427a350d",
+    "url": "/react-milestone-3/static/media/pig.0ed03c8c.png"
+  },
+  {
+    "revision": "cf9c03a20bc51a2e4a97287d53e95e87",
+    "url": "/react-milestone-3/static/media/rabbit.cf9c03a2.png"
+  },
+  {
+    "revision": "9d295dc47f0afa413fbbf02b8a9e2ea5",
+    "url": "/react-milestone-3/static/media/tiger.9d295dc4.png"
+  }
+]);
