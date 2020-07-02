02ecee521570bd9e3b50028e3dfb5774d55e2b55
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-07-02 14:40:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from "./components/Board/Board";
+import backImg from "./assets/images/question-mark.png";
+import monkey from "./assets/images/monkey.png";
+import fox from "./assets/images/fox.png";
+import cow from "./assets/images/cow.png";
+import dog from "./assets/images/dog.png";
+import rabbit from "./assets/images/rabbit.png";
+import tiger from "./assets/images/tiger.png";
+import pig from "./assets/images/pig.png";
+import mouse from "./assets/images/mouse.png";
 
 function App() {
+  const cards = buildCards();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board cards={cards} />
     </div>
   );
 }
 
 export default App;
+
+function buildCards() {
+  let id = 0;
+  const images = { monkey, fox, cow, dog, rabbit, tiger, pig, mouse };
+  const cards = Object.keys(images).reduce((result, item) => {
+    const getCard = () => ({
+      id: id++,
+      type: item,
+      backImg,
+      frontImg: images[item],
+      flipped: false,
+    });
+    return [...result, getCard(), getCard()];
+  }, []);
+  return shuffle(cards);
+}
+
+function shuffle(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let randomIdx = Math.floor(Math.random() * len);
+    let copyCurrent = { ...arr[i] };
+    let copyRandom = { ...arr[randomIdx] };
+    arr[i] = copyRandom;
+    arr[randomIdx] = copyCurrent;
+  }
+  return arr;
+}
